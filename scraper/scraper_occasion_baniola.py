e7202a9c4cac0851d9f6a47ea223de6066cc7157
siwarZidi <siwar.zidi@insat.ucar.tn>
2024-12-12 11:21:15 +0100

    hhhhhh

--- scraper/scraper_occasion_baniola.py
+++ scraper/scraper_occasion_baniola.py
@@ -1,15 +1,14 @@
 import re
+import os  # Import os to handle directories
 import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+url_g = "https://baniola.tn/voitures"
 
-url_g="https://baniola.tn/voitures"
-
-# nbre_page=39
-nbre_page=2
-n=0
+# Number of pages to scrape
+nbre_page = 25
 
 marques = []
 modeles = []
@@ -21,24 +20,23 @@ boites = []
 fuels = []  
 cylindres = []
 nbre_portes = []
-prices=[]
 negotiables = []
 total_df = pd.DataFrame()
 
+# Directory to save the CSV
+output_directory = '../data'
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
 for n in range(nbre_page):
+    url = url_g + "?page=" + str(n * 50)
+    code_page = requests.get(url)
+    soup = BeautifulSoup(code_page.text, 'html.parser')
 
-    url=url_g+"?page="+str(n*50)
+    print('The URL now is:', url)
 
-    code_page=requests.get(url)
-
-    soup=BeautifulSoup(code_page.text,'html.parser')
-
-    print('the url now is :',url)
-    
-    #extraire les liens des voitures pour plus de details:
-    links_container=soup.findAll('div',class_='card-body')
-    # print(links_container[0])
-
+    # Extract car links for details
+    links_container = soup.findAll('div', class_='card-body')
 
     links = []
     for item in links_container:
@@ -46,26 +44,22 @@ for n in range(nbre_page):
         if link_tag:
             link = link_tag['href']
             links.append(link)
-    
 
-    # extraire les features des voitures à partir de la page details de chaque voiture + les prix:
- 
+    # Extract car features and prices
     for link in links:
         print('-------------------')
-        page_voiture=requests.get(link)
-        soup_voiture=BeautifulSoup(page_voiture.text,'html.parser')
+        page_voiture = requests.get(link)
+        soup_voiture = BeautifulSoup(page_voiture.text, 'html.parser')
 
-        prices_container=soup_voiture.findAll('div',class_='price')
+        prices_container = soup_voiture.findAll('div', class_='price')
         for price in prices_container:
             price_text = price.text.strip()
-            # Utiliser une expression régulière pour extraire le nombre
             price_number = re.search(r'\d+', price_text.replace('TND', '').replace(' ', ''))
             if price_number:
                 prices.append(price_number.group())
             else:
-                prices.append(np.nan)  
+                prices.append(np.nan)
 
-            # Vérifier la présence de l'état de négociation
             if 'Négociable' in price_text:
                 negotiables.append('Négociable')
             elif 'Non Négociable' in price_text:
@@ -73,16 +67,9 @@ for n in range(nbre_page):
             else:
                 negotiables.append(np.nan)
 
-        # print(prices)
-        # print(negotiables)
-
-        liste_features=soup_voiture.findAll('div',class_='value')
-        # print(liste_features.__len__())
+        liste_features = soup_voiture.findAll('div', class_='value')
         for i in range(len(liste_features)):
-
-            feature=liste_features[i].text.strip()
-            # print(feature)
-
+            feature = liste_features[i].text.strip()
             if feature:
                 if i == 0:
                     marques.append(feature)
@@ -122,32 +109,33 @@ for n in range(nbre_page):
                 elif i == 8:
                     nbre_portes.append(np.nan)
 
-     
         if len(cylindres) < len(prices):
             cylindres.append(np.nan)
-            # print(len(cylindres))
         if len(nbre_portes) < len(prices):
             nbre_portes.append(np.nan)
-            # print(len(nbre_portes))
-                
+
+    # Create and append the DataFrame
     if len(prices) == len(marques) == len(modeles) == len(kilometrages) == len(years) == len(boites) == len(puissances_fiscales) == len(fuels) == len(negotiables) == len(cylindres) == len(nbre_portes):
-            data = {
-                'Marque': marques,
-                'Modèle': modeles,
-                'year': years,
-                'puissances_fiscales': puissances_fiscales,
-                'road': kilometrages,
-                'boite': boites,
-                'fuel': fuels,
-                'cylindres': cylindres,
-                'nbre_portes': nbre_portes,
-                'negociable': negotiables,
-                'Prix': prices
-            }
-            df = pd.DataFrame(data)
-            print(df)
+        data = {
+            'Marque': marques,
+            'Modèle': modeles,
+            'year': years,
+            'horsepower': puissances_fiscales,
+            'road': kilometrages,
+            'boite': boites,
+            'fuel': fuels,
+            'cylindrée': cylindres,
+            'nbre_portes': nbre_portes,
+            'Negociable': negotiables,
+            'Prix': prices
             
+            
+        }
+        df = pd.DataFrame(data)
+        print(df)
+
     total_df = pd.concat([total_df, df], ignore_index=True)
 
-    total_df.to_csv('../data/baniola.csv', index=False)
-    print(df)
+# Save the DataFrame to CSV
+total_df.to_csv(os.path.join(output_directory, 'baniola1.csv'), index=False)
+print('Data saved to:', os.path.join(output_directory, 'baniola1.csv'))
